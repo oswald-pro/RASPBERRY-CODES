8ef44591da59c7577331ae6339134a0ef2907444
Oswald MBOUSSA <60357182+oswald-pro@users.noreply.github.com>
2021-06-21 10:37:57 +0000

    Update MicroServo9g.py

--- Motors/MicroServo9g.py
+++ Motors/MicroServo9g.py
@@ -1,1 +1,16 @@
-#
+import RPi.GPIO as GPIO
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(17)
+
+while True:
+  servo.mid()
+  print("mid")
+  servo.min()
+  print("min")
+  sleep(1)
+  servo.mid()
+  print("max")
+  sleep(1)
+  
